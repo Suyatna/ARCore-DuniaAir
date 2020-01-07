6bb15230f975589ed9d03533e7b5fd02b07f3a18
SUYATNA <zuyatna@gmail.com>
2020-01-07 10:54:42 +0700

    bismillah

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuBotton;
+    public GameObject panel;
+    
+    public void ActiveMenu()
+    {
+        menuBotton.SetActive(false);
+        panel.SetActive(true);
+    }
+
+    public void DisableMenu()
+    {
+        menuBotton.SetActive(true);
+        panel.SetActive(false);
+    }
+
+    public void ChangeScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
